6a5afec9bf9ecf8256e438b95ea752927a37ec10
LucasBrugevin <lucasbrugevin@gmail.com>
2020-06-19 23:15:59 -0300

    Mi primer commit!

---
+++ Ejercicio de ejemplo/ejercicioEjemplo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 10; i++)
+    {
+        cout<<"mi vieja mula ya no es lo que era"<<endl;
+    }
+    return 0;
+}
